3a1d9514983af7bf390acde2434a2b29804ca10b
weerachai.jant57@sskru.ac.th <toffee107>
2016-09-09 14:13:38 +0700

    new project

---
+++ Hello_OpenSource/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_OpenSource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Weerachai  jantasila  5711423129");
+
+            Console.ReadKey();
+        }
+    }
+}
